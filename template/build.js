5f45d50fb1b2331d48e80ece6011b96b1a3213cf
Bobby Richter <secretrobotron@gmail.com>
2011-12-14 12:12:34 -0500

    removed reference to gladius and its contributors.

--- template/build.js
+++ template/build.js
@@ -1,5 +1,5 @@
 /**
- * Build profile for gladius. Replaces the use of requirejs with an AMD
+ * Build profile for your project. Replaces the use of requirejs with an AMD
  * loader shim, almond.js, since the built file does not need to use
  * all of requirejs.
  */
@@ -11,12 +11,10 @@
   // This setup assumes CubicVR.js is the built output,
   // so this build file assumes make has already run in CubicVR.js
   paths: {
-    'CubicVR.js/CubicVR': '../external/CubicVR.js/dist/CubicVR',
-    'math': '../external/gladius.math/src'
   },
 
   // Use has branch trimming in the build to remove the document.write
-  // code in src/gladius.js after a minification is done.
+  // code in loader.js after a minification is done.
   has: {
     'source-config': false
   },
@@ -28,26 +26,24 @@
 
   // Target the AMD loader shim as the main module to optimize,
   // so it shows up first in the built file,
-  // since the gladius modules use the define/require APIs that the almond
+  // since modules use the define/require APIs that the almond
   // provides. Path is relative to baseUrl.
   name: '../tools/almond',
 
   // Files to include along with almond. Their nested dependencies will also be
-  // included. Subsystems are listed explicitly because gladius-src.js does not
+  // included. Subsystems are listed explicitly because main.js does not
   // have explicit dependencies for them, but uses them on demand. Also,
-  // gladius.js references gladius-src in a document.write string, so it will
+  // loader.js references main.js in a document.write string, so it will
   // not be found by the AST analysis done in the optimizer.
   include: [
-            'gladius',
-            'gladius-src',
            ],
 
-  // Wraps the built file in a closure and exports gladius as a global.
+  // Wraps the built file in a closure and exports a closure for your project as a global.
   wrap: {
     startFile: 'wrap.start',
     endFile: 'wrap.end'
   },
 
-  // The built gladius.js file for use by web sites.
-  out: '../dist/gladius.js'
+  // The built main.js file for use by web sites.
+  out: '../dist/main.js'
 })
